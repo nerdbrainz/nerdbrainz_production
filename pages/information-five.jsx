5b17ab254f3589b8a41ac1d9fc761c2eeb982af1
Alexander Fazan <alexanderfazan@gmail.com>
2021-08-23 17:30:47 +0100

    Auto stash before merge of "main" and "origin/main"

---
+++ pages/information-five.jsx
@@ -0,0 +1,194 @@
+const informationFive = () => {
+  return (
+    <section
+      className=" body-font bg-custom-color-one min-h-full opacity-90
+    "
+    >
+      <div className="container mx-auto flex px-5 pt-24  md:flex-row flex-col items-center">
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 md:mb-0">
+          <img
+            className="object-cover object-center rounded opacity-75"
+            alt="hero"
+            src="/images/49.png"
+          />
+        </div>
+        <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-custom-color-ten">
+            What Website Is Right For Me?
+          </h1>
+          <p className="mb-8 leading-relaxed text-custom-color-ten">
+            With so much on the market when it comes to choosing a website, is
+            it easy to get lost in all the facts and figures. So let’s make this
+            simple.
+            <br />
+            A websites purpose is to present something that you want to
+            showcase. At one end of that spectrum you have information that you
+            want to share with the world. That could be a portfolio of work, a
+            blog or important points relating to your business, something like
+            contact details, open times, menu and so on. On the other end of the
+            spectrum you can have a large multi levelled website with user
+            logins, the transfer of money and the storage of sensitive
+            information.
+            <br />
+            Despite the seemingly overwhelming depth of the subject there are
+            some common questions that anyone commissioning a website should ask
+            themselves.
+          </p>
+
+          <p className="mb-8 leading-relaxed text-custom-color-ten">
+            1. What future do I see for my business and do I want a website that
+            can grow with it.
+            <br />
+            2. Do I want my website to be a purely informational resource. Or do
+            I want to sell my services or products as well.
+            <br />
+            3. Do I have the skills or desire to take complete control over the
+            website or would I prefer someone else to build and/or manage and
+            make future changes to the site for me.
+            <br />
+            4. Who is the audience that I am trying to target.
+            <br />
+            5. What is my budget and how big an investment will a website be.
+            <br />
+            6. What is unique about my business
+            <br />
+            7. What pages do I need to best convey what I need to showcase
+            <br />
+            8. What Do I want the website to look like
+            <br />
+            9. Who is responsible for content creation
+            <br />
+            10.When do I need my site to go active
+            <br />
+          </p>
+          <br />
+
+          {/* <p className="mb-8 leading-relaxed text-custom-color-ten">
+            A website that is purely designed to share information is often
+            called a static site or a landing page. That is not to say that
+            there is only one page or that the it is not interactive or able to
+            evolve. A site that is built to exchange products and services comes
+            under the ever growing umbrella of E-commerce. A well made
+            E-commerce site will be able to offer the functionality of a static
+            website and the interactivity of an E-commerce platform.
+          </p> */}
+          <br />
+          {/* <h3 className="title-font sm:text-2xl text-3xl mb-4 font-medium text-custom-color-ten"></h3> */}
+          {/* <p className="mb-8 leading-relaxed text-custom-color-ten">
+            There are many ways to build a website. It is possible to go to
+            online and find a service that allows you to build your own site
+            using a template.
+            <br />
+            These small sites often created with Wordpress an open-source
+            content management system written in PHP. Is a proven technology
+            that has been a bench mark since its release in 2003 and now in 2021
+            about 40% of the worlds websites are built with Wordpress
+            technology.
+            <br />
+          </p> */}
+        </div>
+      </div>
+      <div className="flex flex-wrap -mx-1 overflow-hidden mx-5  sm:-mx-1 md:-mx-2 lg:-mx-1 xl:-mx-1 lg:px-8 md:items-start md:text-left items-center text-center">
+        <div className="my-1 px-1 w-full overflow-hidden sm:my-1 sm:px-1 md:my-2 md:px-2 lg:my-1 lg:px-1 xl:my-1 xl:px-1 sm:px-10 text-custom-color-ten  pb-24">
+          {/* <h3 className="title-font sm:text-2xl text-3xl mb-4 font-medium text-custom-color-ten">
+            Further Information Pages
+          </h3> */}
+          <p className="mb-8 leading-relaxed text-custom-color-ten">
+            A website that is purely designed to share information is often
+            called a static site or a landing page. That is not to say that
+            there is only one page or that the it is not interactive or able to
+            evolve. A site that is built to exchange products and services comes
+            under the ever growing umbrella of E-commerce. A well made
+            E-commerce site will be able to offer the functionality of a static
+            website and the interactivity of an E-commerce platform.
+            <br />
+            There are many ways to build a website. It is possible to go to
+            online and find a service that allows you to build your own site
+            using a template.
+            <br />
+            These small sites often created with Wordpress an open-source
+            content management system written in PHP. Is a proven technology
+            that has been a bench mark since its release in 2003 and now in 2021
+            about 40% of the worlds websites are built with Wordpress
+            technology.
+            <br />
+            Though a great option for those wanting to build a small site
+            quickly, it can come with unforeseen limitations down the road and
+            can often be difficult to expand as your business grows. Engaging a
+            software developer will normally mitigate these future obstacles as
+            they should be able to build your perfect website in a way that will
+            allow for maximum potential growth down the road.
+            <br />
+            <br />
+            It is perfectly possible to build your own site that you can sell
+            your product and services with these technologies. However in recent
+            years we have seen the growth of dedicated E-commerce platforms like
+            Shopify and BigCommerce these are offer really advanced technical
+            solutions making it is easier to profit online. They also come with
+            statistical analysis of your product and the market interaction with
+            those products. These sort of statistics are an essential part of
+            knowing how to best positioning yourself online.
+          </p>
+
+          {/* <h3 className="title-font sm:text-2xl text-3xl mb-4 font-medium text-custom-color-ten">
+            The Blog
+          </h3> */}
+          <p className="mb-8 leading-relaxed text-custom-color-ten">
+            Hiring a professional web developer is a great option as they will
+            help you map out your desires and expectations for the website and
+            your online business model. Often just talking to the right website
+            developer or agency you will come away with a clearer picture of
+            what you need in a website and the steps you need to take to realise
+            that goal. The biggest bonus of choosing a developer is that they
+            are normally well versed in several programming languages. So
+            therefore can confidently build a website with fewer limitations.
+          </p>
+
+          <br />
+
+          {/* <h3 className="title-font sm:text-2xl text-3xl mb-4 font-medium text-custom-color-ten">
+            To Summarize
+          </h3> */}
+          <p className="mb-8 leading-relaxed text-custom-color-ten">
+            Which ever route you decide to take when creating a new website the
+            most important thing you can do is try and work out what it is you
+            really need for the greatest growth for your business. Learning as
+            much about what is available to you and what you need. Speak with as
+            many people with successful websites up and running and of course
+            ask a professional web developer what they thing is best for you.
+          </p>
+        </div>
+      </div>
+
+      {/* Start of button Section */}
+      <div className="flex justify-center pb-28">
+        <a href="/portfolio_page" target="_blank">
+          <button className="inline-flex text-white bg-custom-color-six border-0 py-2 px-6 focus:outline-none hover:custom-color-eleven rounded text-lg">
+            Portfolio
+          </button>
+        </a>
+        <a href="/contact" target="_blank">
+          <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-custom-color-six rounded text-lg">
+            Say Hello
+          </button>
+        </a>
+      </div>
+      {/* End of button Section */}
+
+      {/*Start of Moving Background */}
+      <div classNameName="ml-44">
+        <div classNameName="absolute w-2/5 h-32 top-1/3 left-4  bg-custom-color-four rounded-full mix-blend-multiply filter blur-3xl opacity-20 animate-blob animation-delay-2000 z-0 "></div>
+
+        <div classNameName="absolute w-2/5 h-24 top-2/3 left-4  bg-custom-color-six rounded-full mix-blend-multiply filter blur-3xl opacity-20 animate-blob animation-delay-6000"></div>
+
+        <div classNameName="absolute w-2/5 h-24 top-1/2 left-4  bg-custom-color-five rounded-full mix-blend-multiply filter blur-3xl opacity-20 animate-blob animation-delay-4000"></div>
+
+        <div classNameName="absolute w-2/5 h-24 bottom-44 left-4  bg-custom-color-five rounded-full mix-blend-multiply filter blur-3xl opacity-20 animate-blob animation-delay-4000"></div>
+      </div>
+
+      {/* End of Moving Background  */}
+    </section>
+  );
+};
+
+export default informationFive;
