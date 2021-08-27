2ba59d37b1db3754f500f46611245f263daaef14
Jared Womack <womackjw@hotmail.com>
2021-08-27 14:22:13 -0700

    contact page copy

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -48,7 +48,7 @@ const Contact = () => {
             </div>
             <div className="lg:w-1/3 md:w-1/2   flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
               <h2 className="text-3xl mb-5 capitalize">
-                We would love to bring your business online.
+                We would love to help you bring your business online.
               </h2>
               <p className="text-xl">
                 Please get in touch with us by text, phone or email and we will
